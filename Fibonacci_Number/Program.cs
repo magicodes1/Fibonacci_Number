9bc27d87e6c41bd225926e5c6aaeb291b758a5ab
magicodes1 <minhsangandonly1@gmail.com>
2021-08-16 18:26:23 +0700

    Solving fibonacci number

---
+++ Fibonacci_Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Fibonacci_Number
+{
+    class Program
+    {
+
+        static int Get_Fibonacci (int number)
+        {
+            int f0 = 0, f1 = 1;
+
+            for (int i = 2; i <= number; i++)
+            {
+                int savedF1 = f1;
+                f1 = f1 + f0;
+                f0 = savedF1;
+            }
+            return f1;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your number:");
+            int number = int.Parse(Console.ReadLine());
+
+            int fibo = Get_Fibonacci(number);
+
+            Console.WriteLine(fibo);        
+        }
+    }
+}
